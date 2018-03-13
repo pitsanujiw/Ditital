7c67b2274498dab4961a7dae433243fd2f633d2c
pitsanujiw <pitsanujill12@gmail.com>
2018-03-13 18:06:18 +0700

    k

--- src/app/components/certifield-letter/certifield-letter.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-certifield-letter',
-  templateUrl: './certifield-letter.component.html',
-  styleUrls: ['./certifield-letter.component.css']
-})
-export class CertifieldLetterComponent {
-}
